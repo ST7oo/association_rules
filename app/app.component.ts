44dd2e5f20adec220ccd89c54e094d8ff1e97a1d
Rodney <reln13st@gmail.com>
2016-05-23 17:39:29 +0200

    initial angular 2

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: `
+	<h1>Association Rules</h1>
+	`
+})
+export class AppComponent {}
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+// The usual bootstrapping imports
+import { bootstrap }      from '@angular/platform-browser-dynamic';
+
+import { AppComponent }   from './app.component';
+
+bootstrap(AppComponent);
